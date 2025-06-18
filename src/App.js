6ed748d4986aec987ac9fc893e57e766ea9bd847
blakehulett7 <blake.hulett7@gmail.com>
2025-06-18 09:05:03 -0500

    starting over

--- src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return React.createElement("h1", {}, "Jesus is Lord!");
-}
-
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
